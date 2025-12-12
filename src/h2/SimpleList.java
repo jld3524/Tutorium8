4b039d2768b2d1da454f9bcb1f6be1d259671566
jldress <jdress@smail.uni-koeln.de>
2025-12-12 22:10:59 +0100

    neu

--- src/h2/SimpleList.java
+++ src/h2/SimpleList.java
@@ -1,4 +1,3 @@
-
 package h2;
 
 public class SimpleList {
@@ -7,51 +6,55 @@ public class SimpleList {
 
     public SimpleList() {
         head = new Node(Integer.MIN_VALUE); 
-        head.next = null;
     }
 
     public Node getFirst() {
-        return head.next; 
+        return head.next;
     }
 
     public Node getLast() {
         Node current = head.next;
-        if (current == null) return null;
+
+        if (current == null) {
+            return null;
+        }
 
         while (current.next != null) {
             current = current.next;
         }
+
         return current;
     }
 
     public void append(int newValue) {
         Node newNode = new Node(newValue);
 
-        
-        if (head.next == null) {
-            head.next = newNode;
-            return;
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
         }
 
-        Node last = getLast();
-        last.next = newNode;
+        current.next = newNode;
     }
 
     public Node findFirst(int value) {
         Node current = head.next;
+
         while (current != null) {
             if (current.value == value) {
                 return current;
             }
             current = current.next;
         }
+
         return null;
     }
 
     public boolean insertAfter(int preValue, int newValue) {
         Node pos = findFirst(preValue);
+
         if (pos == null) {
-            return false; 
+            return false;
         }
 
         Node newNode = new Node(newValue);
@@ -63,14 +66,15 @@ public class SimpleList {
 
     public boolean delete(int value) {
         Node current = head;
+
         while (current.next != null) {
             if (current.next.value == value) {
-                
                 current.next = current.next.next;
                 return true;
             }
             current = current.next;
         }
+
         return false;
     }
 }
